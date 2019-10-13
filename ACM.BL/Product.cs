e9a92308b2789944793a2671cf0b72d0cabd5dff
Ryan Silva <RyandotSilva@gmail.com>
2019-10-13 14:10:56 -0400

    Add Product methods

--- ACM.BL/Product.cs
+++ ACM.BL/Product.cs
@@ -23,5 +23,24 @@ namespace ACM.BL
         public int ProductId { get; set; }
         public string ProductName { get; set; }
 
+        public Product Retrieve(int productId)
+        {
+            return new Product();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(ProductName)) isValid = false;
+            if (CurrentPrice == null) isValid = false;
+
+            return isValid;
+        }
     }
 }
