71b59c100782a1459fb9f006d3e07127b27c1c53
Ryan Silva <RyandotSilva@gmail.com>
2019-10-13 14:15:35 -0400

    Add Order methods

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -15,5 +15,25 @@ namespace ACM.BL
         }
         public DateTimeOffset? OrderDate { get; set; }
         public int OrderId { get; private set; }
+
+        public Order Retrieve(int orderId)
+        {
+            return new Order();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (OrderDate == null) isValid = false;
+
+            return isValid;
+        }
+
     }
 }
