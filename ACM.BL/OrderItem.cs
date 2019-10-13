1d8e395556b8c2590860e76361ccc13c5c7f7743
Ryan Silva <RyandotSilva@gmail.com>
2019-10-13 14:18:39 -0400

    Add OrderItem methods

--- ACM.BL/OrderItem.cs
+++ ACM.BL/OrderItem.cs
@@ -16,5 +16,25 @@
         public decimal? PurchasePrice { get; set; }
         public int Quantity { get; set; }
 
+        public OrderItem Retrieve(int orderItemId)
+        {
+            return new OrderItem();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (Quantity <= 0) isValid = false;
+            if (ProductId <= 0) isValid = false;
+            if (PurchasePrice == null) isValid = false;
+
+            return isValid;
+        }
     }
 }
