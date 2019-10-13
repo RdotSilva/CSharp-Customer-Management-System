1e949c5b964a7437a13cf36ac26cfa3a3df563f2
Ryan Silva <RyandotSilva@gmail.com>
2019-10-13 14:08:40 -0400

    Add Product properties

--- ACM.BL/Product.cs
+++ ACM.BL/Product.cs
@@ -18,5 +18,10 @@ namespace ACM.BL
             ProductId = productId;
         }
 
+        public decimal? CurrentPrice { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+
     }
 }
