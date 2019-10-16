67bb0e80eee9b9d935865cafafd3a8bcc20e8891
Ryan Silva <RyandotSilva@gmail.com>
2019-10-16 16:36:07 -0400

    Add RetrieveValid test

--- Tests/ACM.BLTest/CustomerRepositoryTest.cs
+++ Tests/ACM.BLTest/CustomerRepositoryTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ACM.BL;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ACM.BLTest
@@ -7,8 +8,25 @@ namespace ACM.BLTest
     public class CustomerRepositoryTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RetrieveValid()
         {
+            // Arrange
+            var customerRepository = new CustomerRepository();
+            var expected = new Customer(1)
+            {
+                EmailAddress = "fbaggins@hobbiton.me",
+                FirstName = "Frodo",
+                LastName = "Baggins"
+            };
+
+            // Act
+            var actual = customerRepository.Retrieve(1);
+
+            // Assert
+            Assert.AreEqual(expected.CustomerId, actual.CustomerId);
+            Assert.AreEqual(expected.EmailAddress, actual.EmailAddress);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
         }
     }
 }
