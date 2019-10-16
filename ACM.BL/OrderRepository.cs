c0922c81558e503bd73714d5aaca77ac71b4633c
Ryan Silva <RyandotSilva@gmail.com>
2019-10-16 16:39:03 -0400

    Add Retrieve method

--- ACM.BL/OrderRepository.cs
+++ ACM.BL/OrderRepository.cs
@@ -1,7 +1,27 @@
-﻿namespace ACM.BL
+﻿using System;
+
+namespace ACM.BL
 {
     public class OrderRepository
     {
+        public Order Retrieve(int orderId)
+        {
+            // Create the instance of the Order class
+            // Pass in the requested Id
+            Order order = new Order(orderId);
 
+            // Code that retrieves the defined order
+
+            // Temporary hard-coded values to return 
+            // a populated order
+            if (orderId == 10)
+            {
+                // Use current year in hard-coded data
+                order.OrderDate = new DateTimeOffset(DateTime.Now.Year, 4, 14, 10, 00, 00,
+                    new TimeSpan(7, 0, 0));
+            }
+
+            return order;
+        }
     }
 }
