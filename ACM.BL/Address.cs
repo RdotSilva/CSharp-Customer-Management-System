3613b9d5cdaf3fa1dee27a75f2a72e8661b67658
Ryan Silva <RyandotSilva@gmail.com>
2019-10-16 16:25:15 -0400

    Add Validate method

--- ACM.BL/Address.cs
+++ ACM.BL/Address.cs
@@ -21,5 +21,14 @@
         public string StreetLine1 { get; set; }
         public string StreetLine2 { get; set; }
 
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (PostalCode == null) isValid = false;
+
+            return isValid;
+        }
+
     }
 }
