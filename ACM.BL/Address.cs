4276bc7be3180734d75b4da77292a0593d5c3605
Ryan Silva <RyandotSilva@gmail.com>
2019-10-16 16:24:37 -0400

    Add properties

--- ACM.BL/Address.cs
+++ ACM.BL/Address.cs
@@ -9,7 +9,17 @@
 
         public Address(int addressId)
         {
-            addressId = addressId;
+            AddressId = addressId;
         }
+
+        public int AddressId { get; private set; }
+        public int AddressType { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string State { get; set; }
+        public string StreetLine1 { get; set; }
+        public string StreetLine2 { get; set; }
+
     }
 }
