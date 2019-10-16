6a933e7f226787a2aaf51d5837e507df03b51621
Ryan Silva <RyandotSilva@gmail.com>
2019-10-16 16:27:43 -0400

    Add Retrieve method

--- ACM.BL/CustomerRepository.cs
+++ ACM.BL/CustomerRepository.cs
@@ -8,5 +8,23 @@ namespace ACM.BL
 {
     public class CustomerRepository
     {
+        public Customer Retrieve(int customerId)
+        {
+            // Create the instance of the Customer class
+            // Pass in the requested id
+            Customer customer = new Customer(customerId);
+
+            // Code that retrieves the defined customer
+
+            // Temporary hard-coded values to return 
+            // a populated customer
+            if (customerId == 1)
+            {
+                customer.EmailAddress = "fbaggins@hobbiton.me";
+                customer.FirstName = "Frodo";
+                customer.LastName = "Baggins";
+            }
+            return customer;
+        }
     }
 }
