1c97a5d99ee419dd71abf4930ede4c954f25b9e8
Ryan Silva <RyandotSilva@gmail.com>
2019-10-16 16:40:16 -0400

    Remove unused methods

--- ACM.BL/Product.cs
+++ ACM.BL/Product.cs
@@ -24,16 +24,6 @@ namespace ACM.BL
         public int ProductId { get; set; }
         public string ProductName { get; set; }
 
-        public Product Retrieve(int productId)
-        {
-            return new Product();
-        }
-
-        public bool Save()
-        {
-            return true;
-        }
-
         public bool Validate()
         {
             var isValid = true;
