4f8d5a34007f3c803b5270f9428ac8eb51a9e758
Ryan Silva <RyandotSilva@gmail.com>
2019-10-16 16:30:45 -0400

    Add CustomerRepositoryTest test class

---
+++ Tests/ACM.BLTest/CustomerRepositoryTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerRepositoryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
