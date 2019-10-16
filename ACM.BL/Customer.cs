f2a8e00d811ad853394f3acafcf3d54b0f764a1e
Ryan Silva <RyandotSilva@gmail.com>
2019-10-16 16:29:19 -0400

    Remove unused methods from Customer class

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -50,21 +50,6 @@ namespace ACM.BL
 
         }
 
-        public Customer Retrieve(int customerId)
-        {
-            return new Customer();
-        }
-
-        public List<Customer> Retrieve()
-        {
-            return new List<Customer>();
-        }
-
-        public bool Save()
-        {
-            return true;
-        }
-
         public bool Validate()
         {
             var isValid = true;
