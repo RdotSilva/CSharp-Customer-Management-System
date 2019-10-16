227c704ef1ad30a542c67ae154754af8ab8cbbcb
Ryan Silva <RyandotSilva@gmail.com>
2019-10-16 16:37:56 -0400

    Add Retrieve method

--- ACM.BL/ProductRepository.cs
+++ ACM.BL/ProductRepository.cs
@@ -2,6 +2,24 @@
 {
     public class ProductRepository
     {
+        public Product Retrieve(int productId)
+        {
+            // Create the instance of the Product class
+            // Pass in the requested Id
+            Product product = new Product(productId);
+
+            // Code that retrieves the defined product
+
+            // Temporary hard-coded values to return 
+            // a populated product
+            if (productId == 2)
+            {
+                product.ProductName = "Sunflowers";
+                product.ProductDescription = "Assorted Size Set of 4 Bright Yellow Mini Sunflowers";
+                product.CurrentPrice = 15.96M;
+            }
+            return product;
+        }
 
     }
 }
