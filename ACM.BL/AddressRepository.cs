b8e2622485c75ddc656391a98409a6bede7f3334
Ryan Silva <RyandotSilva@gmail.com>
2019-10-17 14:07:54 -0400

    Add RetrieveByCustomerId method

--- ACM.BL/AddressRepository.cs
+++ ACM.BL/AddressRepository.cs
@@ -1,4 +1,6 @@
-﻿namespace ACM.BL
+﻿using System.Collections.Generic;
+
+namespace ACM.BL
 {
     public class AddressRepository
     {
@@ -24,6 +26,40 @@
 
             }
             return address;
+        }
+        public IEnumerable<Address> RetrieveByCustomerId(int customerId)
+        {
+            // Code that retrieves the defined addresses
+            // for the customer.
+
+
+            // Temporary hard-coded values to return 
+            // a set of addresses for a customer
+            var addressList = new List<Address>();
+            Address address = new Address(1)
+            {
+                AddressType = 1,
+                StreetLine1 = "Bag End",
+                StreetLine2 = "Bagshot row",
+                City = "Hobbiton",
+                State = "Shire",
+                Country = "Middle Earth",
+                PostalCode = "144"
+            };
+            addressList.Add(address);
+
+            address = new Address(2)
+            {
+                AddressType = 2,
+                StreetLine1 = "Green Dragon",
+                City = "Bywater",
+                State = "Shire",
+                Country = "Middle Earth",
+                PostalCode = "146"
+            };
+            addressList.Add(address);
+
+            return addressList;
         }
 
         public bool Save(Address address)
