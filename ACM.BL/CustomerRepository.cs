38922302d5dc488a9962612dc7bf7ca71b07c598
Ryan Silva <RyandotSilva@gmail.com>
2019-10-17 14:10:29 -0400

    Add AddressRepository property

--- ACM.BL/CustomerRepository.cs
+++ ACM.BL/CustomerRepository.cs
@@ -8,6 +8,11 @@ namespace ACM.BL
 {
     public class CustomerRepository
     {
+        public CustomerRepository()
+        {
+            addressRepository = new AddressRepository();
+        }
+        private AddressRepository addressRepository { get; set; }
         public Customer Retrieve(int customerId)
         {
             // Create the instance of the Customer class
