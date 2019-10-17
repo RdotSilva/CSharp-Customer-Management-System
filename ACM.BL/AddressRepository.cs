69a7a28116996571faed916d9de99f78999e8c94
Ryan Silva <RyandotSilva@gmail.com>
2019-10-17 14:05:43 -0400

    Add Retrieve method

--- ACM.BL/AddressRepository.cs
+++ ACM.BL/AddressRepository.cs
@@ -2,6 +2,28 @@
 {
     public class AddressRepository
     {
+        public Address Retrieve(int addressId)
+        {
+            // Create the instance of the Address class
+            // Pass in the requested Id
+            Address address = new Address(addressId);
 
+            // Code that retrieves the defined address
+
+            // Temporary hard coded values to return 
+            // a populated address
+            if (addressId == 1)
+            {
+                address.AddressType = 1;
+                address.StreetLine1 = "Bag End";
+                address.StreetLine2 = "Bagshot row";
+                address.City = "Hobbiton";
+                address.State = "Shire";
+                address.Country = "Middle Earth";
+                address.PostalCode = "144";
+
+            }
+            return address;
+        }
     }
 }
