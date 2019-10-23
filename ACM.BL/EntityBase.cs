83c4d69bc1dfbcece7b6394afaa10c7a99ef7fcd
Ryan Silva <RyandotSilva@gmail.com>
2019-10-23 17:26:23 -0400

    Add properties

--- ACM.BL/EntityBase.cs
+++ ACM.BL/EntityBase.cs
@@ -11,5 +11,14 @@ namespace ACM.BL
         Active,
         Deleted
     }
-    
+    public abstract class EntityBase
+    {
+        public EntityStateOption EntityState { get; set; }
+        public bool HasChanges { get; set; }
+        public bool IsNew { get; private set; }
+        public bool IsValid => Validate();
+
+        public abstract bool Validate();
+
+    }
 }
