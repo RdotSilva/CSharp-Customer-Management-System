50a4f7a54b0b711ccd5dd9a2d29390d4e26a00b4
Ryan Silva <RyandotSilva@gmail.com>
2019-10-23 17:33:36 -0400

    Update Save method

--- ACM.BL/ProductRepository.cs
+++ ACM.BL/ProductRepository.cs
@@ -20,6 +20,7 @@ namespace ACM.BL
                 product.ProductDescription = "Assorted Size Set of 4 Bright Yellow Mini Sunflowers";
                 product.CurrentPrice = 15.96M;
             }
+
             Object myObject = new Object();
             Console.WriteLine($"Object: {myObject.ToString()}");
             Console.WriteLine($"Product: {product.ToString()}");
@@ -29,10 +30,30 @@ namespace ACM.BL
 
         public bool Save(Product product)
         {
-            // Code that saves the passed in product
+            var success = true;
 
-            return true;
+            if (product.HasChanges)
+            {
+                if (product.IsValid)
+                {
+                    if (product.IsNew)
+                    {
+                        // Call an Insert Stored Procedure
+
+                    }
+                    else
+                    {
+                        // Call an Update Stored Procedure
+                    }
+                }
+                else
+                {
+                    success = false;
+                }
+            }
+
+            return success;
+
         }
-
     }
 }
