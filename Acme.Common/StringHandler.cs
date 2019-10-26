1d6771524d3f049645615c470dee05b78b8d5c4a
Ryan Silva <RyandotSilva@gmail.com>
2019-10-26 13:21:52 -0400

    Add InsertSpaces method

--- Acme.Common/StringHandler.cs
+++ Acme.Common/StringHandler.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Acme.Common
 {
-    public class StringHandler
+    public static class StringHandler
     {
+        public static string InsertSpaces(this string source)
+        {
+            string result = string.Empty;
+
+            if (!String.IsNullOrWhiteSpace(source))
+            {
+                foreach (char letter in source)
+                {
+                    if (char.IsUpper(letter))
+                    {
+                        // Trim any spaces already there
+                        result = result.Trim();
+                        result += " ";
+                    }
+                    result += letter;
+                }
+            }
+            result = result.Trim();
+            return result;
+        }
     }
 }
